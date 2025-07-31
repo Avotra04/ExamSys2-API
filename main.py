8685881214a982a8af6e22bb689c4342b99c5f70
Avotra <hei.avotra@gmail.com>
2025-07-31 08:42:02 +0300

    création de l'html HOME

--- main.py
+++ main.py
@@ -8,3 +8,17 @@ def ping():
     return Response(content="pong", media_type="text/plain", status_code=200)
 
 
+
+@app.get("/home", response_class=HTMLResponse)
+def home():
+    html_content = """
+    <html>
+        <head>
+            <title>Home</title>
+        </head>
+        <body>
+            <h1>Welcome home!</h1>
+        </body>
+    </html>
+    """
+    return HTMLResponse(content=html_content, status_code=200)
