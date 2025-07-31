be09e3f1c715b042f439d0716657c7f9811300c1
Avotra <hei.avotra@gmail.com>
2025-07-31 09:41:26 +0300

    feat: add POST /posts endpoint

--- main.py
+++ main.py
@@ -1,19 +1,24 @@
-from fastapi import FastAPI, Response
+from fastapi import FastAPI, Response, status
 from fastapi.requests import Request
-from fastapi.responses import HTMLResponse
-from fastapi.exceptions import RequestValidationError
-from fastapi import FastAPI
-from fastapi.responses import JSONResponse
+from fastapi.responses import HTMLResponse, JSONResponse
+from starlette.exceptions import HTTPException as StarletteHTTPException
+from pydantic import BaseModel
 from typing import List
 from datetime import datetime
 
 app = FastAPI()
 
+posts_db = []
+
+class Post(BaseModel):
+    author: str
+    title: str
+    content: str
+    creation_datetime: datetime
+
 @app.get("/ping")
 def ping():
     return Response(content="pong", media_type="text/plain", status_code=200)
-
-
 
 @app.get("/home", response_class=HTMLResponse)
 def home():
@@ -29,7 +34,12 @@ def home():
     """
     return HTMLResponse(content=html_content, status_code=200)
 
-@app.exception(StarletteHTTPException)
+@app.post("/posts", status_code=status.HTTP_201_CREATED)
+def create_posts(new_posts: List[Post]):
+    posts_db.extend(new_posts)
+    return posts_db
+
+@app.exception_handler(StarletteHTTPException)
 async def custom_404_handler(request: Request, exc: StarletteHTTPException):
     if exc.status_code == 404:
         return HTMLResponse(
@@ -43,3 +53,7 @@ async def custom_404_handler(request: Request, exc: StarletteHTTPException):
             """,
             status_code=404
         )
+    return JSONResponse(
+        status_code=exc.status_code,
+        content={"message": exc.detail},
+    )
